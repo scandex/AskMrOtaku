1601af651b2ce2c0d70865e0dd7bc3b1ed65d45c
scandex <scandex@DESKTOP-QQJ30IU>
2017-02-23 11:45:13 -0500

    first commit

---
+++ src/main/java/com/scandex/askmrotaku/AskMrOtakuApplication.java
@@ -0,0 +1,12 @@
+package com.scandex.askmrotaku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskMrOtakuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskMrOtakuApplication.class, args);
+	}
+}
