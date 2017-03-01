131cda815fb43ce83c55eb61669e49cc6eb4be23
scandex <scandex@DESKTOP-QQJ30IU>
2017-02-28 22:27:32 -0500

    headers fix

--- src/main/java/com/scandex/askmrotaku/web/RecommendationController.java
+++ src/main/java/com/scandex/askmrotaku/web/RecommendationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +16,7 @@ import com.scandex.askmrotaku.service.IRecommendationService;
 
 @RestController
 @RequestMapping("/recommendation")
+@CrossOrigin(origins = "*")
 public class RecommendationController {
 
 	@Autowired
