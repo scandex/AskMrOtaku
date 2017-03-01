ca5a9ddb95f6c374b50726074af484c5f9bb8fcc
scandex <scandex@DESKTOP-QQJ30IU>
2017-02-28 19:45:38 -0500

    api routes

--- src/main/java/com/scandex/askmrotaku/web/RecommendationController.java
+++ src/main/java/com/scandex/askmrotaku/web/RecommendationController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,19 +21,19 @@ public class RecommendationController {
 	private IRecommendationService recommender;	
 	
 	
-	@RequestMapping("/userBased")
-	public List<Anime> recommendUserBased(@RequestParam(value = "user_id") long id) {
-				return recommender.recommendUserBased(id);
+	@RequestMapping("/userBased/{userId}")
+	public List<Anime> recommendUserBased(@PathVariable long userId) {
+				return recommender.recommendUserBased(userId);
 	}
 	
-	@RequestMapping("/cluster1Based")
-	public List<Anime> recommendCluster1Based(@RequestParam(value = "user_id") long id) {
-				return recommender.recommendC1Based(id);
+	@RequestMapping("/cluster1Based/{userId}")
+	public List<Anime> recommendCluster1Based(@PathVariable long userId) {
+				return recommender.recommendC1Based(userId);
 	}
 	
-	@RequestMapping("/cluster2Based")
-	public List<Anime> recommendCluster2Based(@RequestParam(value = "user_id") long id) {
-				return recommender.recommendC2Based(id);
+	@RequestMapping("/cluster2Based/{userId}")
+	public List<Anime> recommendCluster2Based(@PathVariable long userId) {
+				return recommender.recommendC2Based(userId);
 	}
 	
 	@RequestMapping("/animeToReview")
@@ -40,8 +41,8 @@ public class RecommendationController {
 				return recommender.getAnimeToReview();
 	}
 
-	@RequestMapping("/preference")
-	public void registerPreference(@RequestParam(value = "user_id") long userId,@RequestParam(value = "anime_id") long animeId,@RequestParam(value = "rating") double rating) {
+	@RequestMapping("/preference/{userId}")
+	public void registerPreference(@PathVariable long userId,@RequestParam(value = "anime_id") long animeId,@RequestParam(value = "rating") double rating) {
 		recommender.setPreference(userId, animeId, rating);
 	}
 
@@ -50,9 +51,9 @@ public class RecommendationController {
 		return recommender.getTemporalId();
 	}
 	
-	@RequestMapping("/releaseId")
-	public void releaseId(@RequestParam(value = "user_id") long id) {
-				recommender.releaseTemporalId(id);
+	@RequestMapping("/releaseId/{userId}")
+	public void releaseId(@PathVariable long userId) {
+				recommender.releaseTemporalId(userId);
 	}
 
 }
